974105cf05b212eaa89a77c04e6609c747910569
Tanaka Seigo <seigo@1ft-seabass.jp>
2016-03-21 18:22:51 +0900

    進捗コミット
    Signed-off-by: Tanaka Seigo <seigo@1ft-seabass.jp>

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-MILKCOCOA_APP_ID = "MILKCOCOA_APP_ID";
-MILKCOCOA_DATASTORE_ID = "milkcocoa_vol9_littlebits";
+// Milkcocoa設定
+var MILKCOCOA_APP_ID = "MILKCOCOA_APP_ID";
+var MILKCOCOA_DATASTORE_ID = "milkcocoa_littlebits";
+// Arduinoポート設定
+var portName = 'COM4';  // Arduinoが認識されているポートと一致させましょう
 
 // MilkCocoa /////////////////////////////////
 var MilkCocoa = require("./node_modules/milkcocoa/index.js");
@@ -16,11 +19,11 @@ sampleDataStore.on("push", function(datum) {
 });
 //////////////////////////////////////////////
 // dataStore作成 iftttDataStore
-var iftttDataStore = milkcocoa.dataStore("iftttDataStore");
+var iftttDataStore = milkcocoa.dataStore("IFTTTDataStore");
 // データがpushされたときのイベント通知
 iftttDataStore.on("push", function(datum) {
     // 内部のログ
-    console.log('[push complete] iftttDataStore');
+    console.log('[push complete] IFTTTDataStore');
     console.log(datum);
     // 出力ポート2が反応する
     sp.write(2,function(err,results){
@@ -34,6 +37,8 @@ iftttDataStore.on("push", function(datum) {
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var sp;
+
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -48,7 +53,7 @@ app.get('/', function(request, response) {
 app.get('/port', function(request, response) {
     response.send('[get]/port');
     // 出力ポート2が反応する
-    sp.write(2,function(err,results){
+    sp.write("2",function(err,results){
         console.log('-- socket.io output sp.write complete');
         console.log(err);
         console.log(results);
@@ -73,11 +78,12 @@ app.listen(app.get('port'), function() {
     // サーバーの起動
     console.log("Node app is running at localhost:" + app.get('port'));
 
+    sampleDataStore.push({content : 'Hello!!!'});
+
     // Serial Port設定 ///////////////////////////////
 
     var serialport = require('serialport');
-    var portName = 'COM4';  // Arduinoが認識されているポートと一致させましょう
-    var sp = new serialport.SerialPort(portName, {
+    sp = new serialport.SerialPort(portName, {
         baudRate: 9600,
         dataBits: 8,
         parity: 'none',
@@ -94,15 +100,15 @@ app.listen(app.get('port'), function() {
             if( buffer == 1 ){
                 // 1番目のポートのlittleBits入力の情報を送る
                 console.log('-> LittlebitsInputAction 1');
-                sampleDataStore.push({ port : 1 , host : request.headers.host });
+                sampleDataStore.push({ name : 'LittlebitsInputAction' , port : 1 });
             } else if( buffer == 2 ){
                 // 2番目のポートのlittleBits入力の情報を送る
                 console.log('-> LittlebitsInputAction 2');
-                sampleDataStore.push({ port : 2 , host : request.headers.host });
+                sampleDataStore.push({ name : 'LittlebitsInputAction' , port : 2 });
             } else if( buffer == 3 ){
                 // 3番目のポートのlittleBits入力の情報を送る
                 console.log('-> LittlebitsInputAction 3');
-                sampleDataStore.push({ port : 3 , host : request.headers.host });
+                sampleDataStore.push({ name : 'LittlebitsInputAction' , port : 3 });
             }
         } catch(e) {
             return;
